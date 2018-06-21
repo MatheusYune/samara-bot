213846b5ac42f4e023ed0a85f62d7e07d5f51415
MatheusYune <matheusyune@gmail.com>
2018-06-21 14:27:50 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const ID_DBREAKER = "230878441006825474"
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
@@ -12,13 +11,4 @@ client.on('message', msg => {
   }
 });
 
-function calculaChances() {
-    
-}
-
-client.on('message', (message) => {
-    if (message.author.id == ID_DBREAKER && calculaChances()) {
-        message.channel.send(" Nadavê issaí que tu falou, nem existe counter pick")
-    }
-});
 client.login(process.env.BOT_TOKEN);
